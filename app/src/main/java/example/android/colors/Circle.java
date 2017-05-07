71784cf0ffa90375f04e9fd2ee08b633a48355df
jansplichal <splichal.jan@gmail.com>
2017-05-07 22:43:08 +0200

    Final app

---
+++ app/src/main/java/example/android/colors/Circle.java
@@ -0,0 +1,13 @@
+package example.android.colors;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Circle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
+    }
+}
